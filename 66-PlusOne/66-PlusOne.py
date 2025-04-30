4b640def176cc6b88eff38d0d6220ffa62a7955d
Kavish Shah <76742201+thekavishshah@users.noreply.github.com>
2025-04-30 01:33:56 -0700

    Create file

---
+++ 66-PlusOne/66-PlusOne.py
@@ -0,0 +1,19 @@
+# Last updated: 4/30/2025, 1:33:56 AM
+class Solution(object):
+    def plusOne(self, digits):
+        """
+        :type digits: List[int]
+        :rtype: List[int]
+        """
+        for i in range(len(digits)-1, -1, -1):
+            if digits[i] < 9:
+                digits[i] += 1
+                return digits
+            digits[i] = 0
+        return [1] + digits
+
+
+
+            
+
+        
