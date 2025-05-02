f8383df8b6e6df320028e0f293a0ba08c92bf772
Kavish Shah <76742201+thekavishshah@users.noreply.github.com>
2025-05-02 13:02:28 -0700

    Create file

---
+++ 9-PalindromeNumber/9-PalindromeNumber.py
@@ -0,0 +1,11 @@
+# Last updated: 5/2/2025, 1:02:27 PM
+class Solution(object):
+    def isPalindrome(self, x):
+        bro=str(x)
+        return bro==bro[::-1]
+        """
+        :type x: int
+        :rtype: bool
+        """
+    
+        
