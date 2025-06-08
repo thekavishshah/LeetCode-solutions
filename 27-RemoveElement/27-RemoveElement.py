385e3199cb971002b1cd2ae809ead53bcbd3cbe7
Kavish Shah <76742201+thekavishshah@users.noreply.github.com>
2025-06-07 19:02:43 -0700

    Create 27-RemoveElement

---
+++ 27-RemoveElement/27-RemoveElement.py
@@ -0,0 +1,15 @@
+# Last updated: 6/7/2025, 7:02:43 PM
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        k=0
+        for i in range(len(nums)):
+            if nums[i]!=val:
+                nums[k]=nums[i]
+                k+=1
+
+        return k
