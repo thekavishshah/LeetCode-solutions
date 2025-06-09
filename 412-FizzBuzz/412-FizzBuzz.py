a8fcb744475340a8786cc0f41f01e0db9d2dab91
Kavish Shah <76742201+thekavishshah@users.noreply.github.com>
2025-06-09 01:38:28 -0700

    File updated at 6/9/2025, 1:38:28 AM

--- 412-FizzBuzz/412-FizzBuzz.py
+++ 412-FizzBuzz/412-FizzBuzz.py
@@ -1,9 +1,9 @@
-# Last updated: 6/9/2025, 1:37:45 AM
+# Last updated: 6/9/2025, 1:38:28 AM
 class Solution(object):
     def fizzBuzz(self, n):
         result = []
         for i in range(1, n + 1):
-            if i % 3 == 0 and i % 5 == 0:
+            if i % 15 ==0:
                 result.append("FizzBuzz")
             elif i % 3 == 0:
                 result.append("Fizz")
