4bfde3d806b550a372c58901c0f5ab3f40df72e7
Kavish Shah <76742201+thekavishshah@users.noreply.github.com>
2025-07-16 16:56:36 -0700

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/709-ToLowerCase.py
@@ -0,0 +1,9 @@
+# Last updated: 7/16/2025, 4:56:36 PM
+class Solution(object):
+    def toLowerCase(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        a=lower(s)
+        return a
