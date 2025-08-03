e7865d639c5c2e634e6b0a68e36091ae6880a39f
Kavish Shah <76742201+thekavishshah@users.noreply.github.com>
2025-08-03 00:28:58 -0700

    File updated at 8/3/2025, 12:28:58 AM

--- 1550-ThreeConsecutiveOdds/1550-ThreeConsecutiveOdds.py
+++ 1550-ThreeConsecutiveOdds/1550-ThreeConsecutiveOdds.py
@@ -1,17 +1,12 @@
-# Last updated: 8/3/2025, 12:27:16 AM
+# Last updated: 8/3/2025, 12:28:58 AM
 class Solution(object):
     def threeConsecutiveOdds(self, arr):
         """
         :type arr: List[int]
         :rtype: bool
         """
-        count=0
-        for i in range(0,len(arr)):
-            if arr[i]%2==1:
-                count+=1
-
-                if count==3:
-                    return True
-            else:
-                count=0
+        for i in range(0,len(arr)-2):
+            if arr[i]%2==1 and arr[i+1]%2==1 and arr[i+2]%2==1:
+                return True
         return False
+        
