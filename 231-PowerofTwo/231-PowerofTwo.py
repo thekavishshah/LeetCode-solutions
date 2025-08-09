00c7e637ce88b9cd56233fbb33ddfeb93af9f61e
Kavish Shah <76742201+thekavishshah@users.noreply.github.com>
2025-08-08 17:17:47 -0700

    File updated at 8/8/2025, 5:17:47 PM

--- 231-PowerofTwo/231-PowerofTwo.py
+++ 231-PowerofTwo/231-PowerofTwo.py
@@ -1,9 +1,10 @@
-# Last updated: 8/8/2025, 5:17:25 PM
-class Solution(object):
-    def isPowerOfTwo(self, n):
+# Last updated: 8/8/2025, 5:17:47 PM
+class Solution:
+    def isPowerOfTwo(self, n: int) -> bool:
         if n==1:
             return True
         for i in range(32):
             if 2**i==n:
                 return True
         return False
+        
